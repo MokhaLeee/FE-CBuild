8659ca771fafdf08f136e25dc1a528dcf6a475d9
MokhaLeee <nkulyc@163.com>
2023-08-18 11:04:41 +0000

    ems done

--- Wizardry/SaveData/Source/MsuFunc.c
+++ Wizardry/SaveData/Source/MsuFunc.c
@@ -1,9 +1,142 @@
 #include "common-chax.h"
+#include "bmunit.h"
 #include "bmsave.h"
 #include "sram-layout.h"
+#include "uimenu.h"
+#include "bmmind.h"
+#include "event.h"
+
+#include "debug-kit.h"
 #include "save-data.h"
 
-void MSA_SavePlaySt(u8 *dst, const u32 size)
+void MSU_SavePlaySt(u8 * dst, const u32 size)
 {
+    gPlaySt.time_saved = GetGameClock();
+    WriteAndVerifySramFast(&gPlaySt, dst, sizeof(gPlaySt));
+}
 
+void MSU_LoadPlaySt(u8 * src, const u32 size)
+{
+    ReadSramFast(src, &gPlaySt, sizeof(gPlaySt));
+    SetGameTime(gPlaySt.time_saved);
+}
+
+void MSU_SaveAction(u8 * dst, const u32 size)
+{
+    StoreRNStateToActionStruct();
+    WriteAndVerifySramFast(&gActionData, dst, sizeof(struct ActionData));
+}
+
+void MSU_LoadAction(u8 * src, const u32 size)
+{
+    ReadSramFast(src, &gActionData, sizeof(struct ActionData));
+    LoadRNStateFromActionStruct();
+}
+
+void MSU_SaveMenuMask(u8 * dst, const u32 size)
+{
+    u8 list[MENU_OVERRIDE_MAX];
+    GetForceDisabledMenuItems(list);
+    WriteAndVerifySramFast(list, dst, sizeof(list));
+}
+
+void MSU_LoadMenuMask(u8 * src, const u32 size)
+{
+    u8 list[MENU_OVERRIDE_MAX];
+    ReadSramFast(src, list, sizeof(list));
+    SetForceDisabledMenuItems(list);
+}
+
+void MSU_SaveWorldMap(u8 * dst, const u32 size)
+{
+    WriteWorldMapStuff(dst, &gGMData);
+}
+
+void MSU_LoadWorldMap(u8 * src, const u32 size)
+{
+    ReadWorldMapStuff(src, &gGMData);
+}
+
+void MSU_SaveDungeon(u8 * dst, const u32 size)
+{
+    struct Dungeon dungeon[2];
+    SaveDungeonRecords(dungeon);
+    WriteAndVerifySramFast(dungeon, dst, sizeof(dungeon));
+}
+
+void MSU_LoadDungeon(u8 * src, const u32 size)
+{
+    struct Dungeon dungeon[2];
+    ReadSramFast(src, dungeon, sizeof(dungeon));
+    LoadDungeonRecords(dungeon);
+}
+
+void MSU_SaveDungeon2(u8 * dst, const u32 size)
+{
+    struct Dungeon dungeon;
+    SaveDungeonState(&dungeon);
+    WriteAndVerifySramFast(&dungeon, dst, sizeof(struct Dungeon));
+}
+
+void MSU_LoadDungeon2(u8 * src, const u32 size)
+{
+    struct Dungeon dungeon;
+    ReadSramFast(src, &dungeon, sizeof(struct Dungeon));
+    LoadDungeonState(&dungeon);
+}
+
+void MSU_SaveEvtCounter(u8 * dst, const u32 size)
+{
+    int val = GetEventSlotCounter();
+    WriteAndVerifySramFast(&val, dst, sizeof(int));
+}
+
+void MSU_LoadEvtCounter(u8 * src, const u32 size)
+{
+    int val;
+    ReadSramFast(src, &val, sizeof(int));
+    SetEventSlotCounter(val);
+}
+
+void MSU_SaveBonusClaim(void)
+{
+    return;
+}
+
+void MSU_LoadBonusClaimWIP(void)
+{
+    u32 buf;
+    void * src = GetSaveReadAddr(gPlaySt.gameSaveSlot);
+    ReadSramFast(src + 0x0638, &buf, sizeof(buf));  /* 0x0638 is countered from gEmsSavChunks */
+    SetBonusContentClaimFlags(buf);
+}
+
+void MSU_SaveBlueUnits(u8 * dst, const u32 size)
+{
+    return;
+}
+
+void MSU_LoadBlueUnits(u8 * src, const u32 size)
+{
+    return;
+}
+
+void MSU_SaveRedUnits(u8 * dst, const u32 size)
+{
+    return;
+}
+
+void MSU_LoadRedUnits(u8 * src, const u32 size)
+{
+    return;
+}
+
+void MSU_SaveGreenUnits(u8 * dst, const u32 size)
+{
+    return;
+}
+
+void MSU_LoadGreenUnits(u8 * src, const u32 size)
+{
+    return;
 }
--- include/save-data.h
+++ include/save-data.h
@@ -5,18 +5,25 @@
 #include "bmitem.h"
 #include "sram-layout.h"
 
+#define EMS_CHUNK_INVALID_OFFSET ((u16)-1)
+
 struct EmsChunk {
-    /* 00 */ u16 off;
+    /* 00 */ u16 offset;
     /* 02 */ u16 size;
 
     /* 04 */ void (* save)(u8 * dst, const u32 size);
     /* 08 */ void (* load)(u8 * src, const u32 size);
 };
 
-extern const struct EmsChunk gEmsSaveChunks[];
+extern const struct EmsChunk gEmsSavChunks[];
 extern const struct EmsChunk gEmsSusChunks[];
+extern const u16 gEmsOffsets[SAVE_ID_MAX];
 
-struct EmsPackedUnit {
+extern const u16 gEmsSizes[2];
+#define EMS_SIZE_SAV (gEmsSizes[0])
+#define EMS_SIZE_SUS (gEmsSizes[1])
+
+struct EmsPackedSavUnit {
     /* 00 */ u8 jid;
     /* 01 */ u8 pid;
     /* 02 */ u8 max_hp;
@@ -25,15 +32,40 @@ struct EmsPackedUnit {
              u32 exp   : 7;
              u32 xPos  : 6;
              u32 yPos  : 6;
-    /* 0F */ u8 wranks[0x8];
-    /* 17 */ u8 supports[UNIT_SUPPORT_MAX_COUNT];
+    /* 0F */ u8 ranks[0x8];
+    /* 17 */ u8 skills[7];
     /* 1E */ u16 items[UNIT_ITEM_COUNT];
     /* 28 */ u32 state;
     /* 2C */
 } BITPACKED;
 
-struct EmsPackedUnitSusAlly {
-    /* 00 */ struct EmsPackedUnit unitp;
+struct EmsPackedSusUnit {
+    /* 00 */ u8 jid;
+    /* 01 */ u8 pid;
+    /* 02 */ u8 max_hp;
+    /* 03 */ u8 pow, mag, skl, spd, lck, def, res, mov, con;
+    /* 0C */ u32 level : 5;
+             u32 exp   : 7;
+             u32 xPos  : 6;
+             u32 yPos  : 6;
+    /* 0F */ u8 ranks[0x8];
+
+    /* 17 */ union {
+                u8 skills[7];
+
+                struct {
+                    u8 ai1;
+                    u8 ai2;
+                    u8 ai1_cur;
+                    u8 ai2_cur;
+                    u8 ai_flag;
+                    u16 ai_config;
+                } ai;
+            } pad;
+
+    /* 1E */ u16 items[UNIT_ITEM_COUNT];
+    /* 28 */ u32 state;
+    /* 2C */
     /* 2C */ u8 cur_hp;
     /* 2D */ u8 rescue;
     /* 2E */ u8 ballista;
@@ -42,25 +74,5 @@ struct EmsPackedUnitSusAlly {
     /* 30 */ u8 torch    : 4;
              u8 barrier  : 4;
 
-    /* 31 */ u8 _pad_[3];
-    /* 34 */
-} BITPACKED;
-
-struct EmsPackedUnitSusAi {
-    /* 00 */ struct EmsPackedUnit unitp;
-    /* 2C */ u8 cur_hp;
-    /* 2D */ u8 rescue;
-    /* 2E */ u8 ballista;
-    /* 2F */ u8 status   : 4;
-             u8 duration : 4;
-    /* 30 */ u8 torch    : 4;
-             u8 barrier  : 4;
-
-    /* 31 */ u8 ai1;
-    /* 32 */ u8 ai2;
-    /* 33 */ u8 ai1_cur;
-    /* 34 */ u8 ai2_cur;
-    /* 35 */ u8 ai_flag;
-    /* 36 */ u16 ai_config;
-    /* 38 */
+    /* 31 */
 } BITPACKED;
