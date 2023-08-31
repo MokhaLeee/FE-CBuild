257f652aafc43db0e4bbe3e303dbad59251d9a64
MokhaLeee <nkulyc@163.com>
2023-08-31 09:14:42 +0000

    add str-mag infos, add loadunit hook

--- include/strmag.h
+++ include/strmag.h
@@ -11,6 +11,16 @@
 #define UNIT_MAG(unit) ((unit)->_u3A)
 #define BU_CHG_MAG(bu) (*((u8 *)(bu) + 0x7F))
 
+struct UnitMagicInfo {
+    s8 base;
+    s8 growth;
+    s8 cap;
+    s8 bonus;
+};
+
+extern const struct UnitMagicInfo gMagicPInfos[0x100];
+extern const struct UnitMagicInfo gMagicJInfos[0x100];
+
 static inline bool IsMagicAttack(struct BattleUnit * bu)
 {
     return !!((IA_MAGICDAMAGE | IA_MAGIC) & bu->weaponAttributes);
@@ -18,12 +28,22 @@ static inline bool IsMagicAttack(struct BattleUnit * bu)
 
 static inline int GetUnitMaxMagic(struct Unit * unit)
 {
-    return 30;
+    int status = gMagicJInfos[UNIT_CLASS_ID(unit)].cap;
+
+    if (status <= 0)
+        return 20;
+
+    return status;
 }
 
 static inline int GetUnitBasicMagGrowth(struct Unit * unit)
 {
-    return 50;
+    return gMagicPInfos[UNIT_CHAR_ID(unit)].growth;
+}
+
+static inline int GetClassChgMagicBonus(u8 jid)
+{
+    return gMagicJInfos[jid].bonus;
 }
 
 static inline int GetUnitMagic(struct Unit * unit)
