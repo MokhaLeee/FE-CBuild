a8d162ad65ad38013bf33a55bfcecf852f02c29b
MokhaLeee <nkulyc@163.com>
2023-09-08 17:35:51 +0000

    weapon range string: move to bottom

--- Wizardry/WeaponRange/Source/RangeString.c
+++ Wizardry/WeaponRange/Source/RangeString.c
@@ -8,18 +8,18 @@
 
 extern struct MsgBuffer sMsgString;
 
-/* 996 ---> 9 9 6 */
-STATIC_DECLAR void SplitNumberDec3(int in, u8 out[])
+/* 96 ---> 9 6 */
+STATIC_DECLAR void SplitNumberDec2(int in, u8 out[])
 {
-    out[0] = Div(in, 100);
-    out[1] = Div(in - out[0] * 100, 10);
-    out[2] = in - out[0] * 100 - out[1] * 10;
+    out[0] = Div(in, 10);
+    out[1] = in - out[0] * 10;
 }
 
 STATIC_DECLAR char * ItemRangeToString(int min, int max)
 {
     int i, count = 0;
-    u8 min_splited[3], max_splited[3];
+    u8 min_splited[2], max_splited[2];
+    char str[5];
 
     if (max == 0)
         return GetStringFromIndex(0x522); /* Mag / 2[X] */
@@ -27,29 +27,36 @@ STATIC_DECLAR char * ItemRangeToString(int min, int max)
     if (min > max || min < 0 || max < 0)
         return GetStringFromIndex(0x52B); /* "--\0" */
 
-    SplitNumberDec3(min, min_splited);
-    SplitNumberDec3(max, max_splited);
+    SplitNumberDec2(min, min_splited);
+    SplitNumberDec2(max, max_splited);
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < 2; i++)
         if (min_splited[i] != 0)
             break;
 
-    for (; i < 3; i++)
-        sMsgString.buffer1[count++] = '0' + min_splited[i];
+    for (; i < 2; i++)
+        str[count++] = '0' + min_splited[i];
 
     if (max > min)
     {
-        sMsgString.buffer1[count++] = '-';
+        str[count++] = '-';
 
-        for (i = 0; i < 3; i++)
+        for (i = 0; i < 2; i++)
             if (max_splited[i] != 0)
                 break;
 
-        for (; i < 3; i++)
-            sMsgString.buffer1[count++] = '0' + max_splited[i];
+        for (; i < 2; i++)
+            str[count++] = '0' + max_splited[i];
     }
 
-    sMsgString.buffer1[count] = '\0';
+    for (i = 0; i < 5; i++)
+    {
+        if ((i + count) < 5)
+            sMsgString.buffer1[i] = ' ';
+        else
+            sMsgString.buffer1[i] = str[i + count - 5];
+    }
+    sMsgString.buffer1[5] = '\0';
     return (char *)sMsgString.buffer1;
 }
 
@@ -77,12 +84,14 @@ const char * GetUnitDisplayRangeString(struct Unit * unit)
             return GetStringFromIndex(0x52B); /* "--\0" */
     }
 
-    item = GetUnitEquippedWeaponSlot(unit);
+    item = GetUnitEquippedWeapon(unit);
     if ((ITEM_INDEX(item) == 0))
         return GetStringFromIndex(0x52B); /* "--\0" */
 
     min = GetItemMinRangeRework(item, unit);
     max = GetItemMaxRangeRework(item, unit);
+
+    LogDebugf("%s: %d - %d, item %#x", __func__, min, max, item);
 
     return ItemRangeToString(min, max);
 }
