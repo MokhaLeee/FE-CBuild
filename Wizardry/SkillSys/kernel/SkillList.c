4272594f3b851ad2a3a32e69102a0f63321da818
MokhaLeee <nkulyc@163.com>
2023-09-15 11:29:55 +0000

    fix on skill-list

--- Wizardry/SkillSys/kernel/SkillList.c
+++ Wizardry/SkillSys/kernel/SkillList.c
@@ -14,17 +14,34 @@ extern u32 sSkillListNext;
  */
 extern struct SkillList sSkillLists[4];
 
+STATIC_DECLAR struct SkillList * GetExistingSkillList(struct Unit * unit)
+{
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        if (unit->index != sSkillLists[i].uid)
+            continue;
+
+        switch (i) {
+        case 0:
+        case 1:
+            sSkillListNext = !i;
+            break;
+        }
+        return &sSkillLists[i];
+    }
+    return NULL;
+}
+
 struct SkillList * GetUnitSkillList(struct Unit * unit)
 {
     int i;
-    struct SkillList * list = NULL;
-
-    for (i = 3; i >= 0; i--)
-        if (unit->index == sSkillLists[i].uid)
-            list = sSkillLists + i;
+    struct SkillList * list = GetExistingSkillList(unit);
 
     if (!list)
     {
+        Debugf("Generate skill for %#x, next %d", UNIT_CHAR_ID(unit), sSkillListNext);
+
         if (unit->index == gBattleActor.unit.index)
             list = &sSkillLists[3];
         else if (unit->index == gBattleTarget.unit.index)
