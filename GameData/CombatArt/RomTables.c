556b2d068688ccc24f05c449868dbb379c2c4d2c
MokhaLeee <nkulyc@163.com>
2023-09-19 12:43:55 +0000

    set combat default arts, fix text issues

--- GameData/CombatArt/RomTables.c
+++ GameData/CombatArt/RomTables.c
@@ -49,15 +49,20 @@ FEB_IDENTIFIER(gCombatArtRomJTable);
 
 const struct CombatArtRomTable gCombatArtDefaultTable = {
     .cid_sword = {
-        [WPN_LEVEL_D] = CID_Grounder,
-        [WPN_LEVEL_C] = CID_BaneOfMonsters,
+        [WPN_LEVEL_D] = CID_WrathStrike,
+        [WPN_LEVEL_C] = CID_Soulblade,
+        [WPN_LEVEL_B] = CID_FoudroyantStrike,
     },
     .cid_lance = {
         [WPN_LEVEL_D] = CID_TempestLance,
-        [WPN_LEVEL_C] = CID_SwiftStrikes,
+        [WPN_LEVEL_C] = CID_MonsterPiercer,
+        [WPN_LEVEL_B] = CID_FrozenLance,
     },
     .cid_axe = {
-        [WPN_LEVEL_C] = CID_HelmSplitter,
+        [WPN_LEVEL_D] = CID_Smash,
+        [WPN_LEVEL_C] = CID_FocusedStrike,
+        [WPN_LEVEL_B] = CID_HelmSplitter,
+        [WPN_LEVEL_S] = CID_FlickeringFlower,
     },
     .cid_bow = {
         [WPN_LEVEL_D] = CID_CurvedShot,
