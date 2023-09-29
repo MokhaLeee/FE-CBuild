631a25efc05f9e3914181f1081477e11208b128f
MokhaLeee <nkulyc@163.com>
2023-09-29 08:54:43 +0000

    add prologue event, add portraits, add character configs

---
+++ include/constants/chax-characters.h
@@ -0,0 +1,15 @@
+#ifndef CHAX_CHARACTERS
+#define CHAX_CHARACTERS
+
+#define CHAR_Lysithea       0x01
+#define CHAR_Bernadetta     0x02
+#define CHAR_Hapi           0x03
+#define CHAR_Mercedes       0x04
+#define CHAR_Felix          0x05
+#define CHAR_Dimitri        0x06
+#define CHAR_Eru            0x07
+
+#define CHAR_Ch0Boss        0x68 // ONeill
+#define CHAR_Ch0Generic     0x80
+
+#endif
