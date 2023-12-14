21939f75bf19a1711c3cd9e0c370eb6bcd5dab71
MokhaLeee <nkulyc@163.com>
2023-12-14 11:00:18 +0000

    fix strange compling bug

--- Wizardry/Common/UnitExpa/Source/UnitExpa.c
+++ Wizardry/Common/UnitExpa/Source/UnitExpa.c
@@ -33,35 +33,25 @@ bool CheckBitUES(struct Unit * unit, int bit)
     return false;
 }
 
+static void reset_expa(s8 uid)
+{
+    struct Unit * unit = GetUnit(uid);
+    if (!unit)
+        return;
+
+    unit->_u3A = 0;
+    unit->_u3B = 0;
+}
+
 void ResetUnitsExpaSus(void)
 {
     int i;
-    struct Unit * unit;
     for (i = FACTION_BLUE + 1; i < (FACTION_BLUE + 0x40); i++)
-    {
-        unit = GetUnit(i);
-        if (unit)
-            continue;
+        reset_expa(i);
 
-        unit->_u3A = 0;
-        unit->_u3B = 0;
-    }
     for (i = FACTION_RED + 1; i < (FACTION_RED + 0x40); i++)
-    {
-        unit = GetUnit(i);
-        if (unit)
-            continue;
+        reset_expa(i);
 
-        unit->_u3A = 0;
-        unit->_u3B = 0;
-    }
     for (i = FACTION_GREEN + 1; i < (FACTION_GREEN + 0x40); i++)
-    {
-        unit = GetUnit(i);
-        if (unit)
-            continue;
-
-        unit->_u3A = 0;
-        unit->_u3B = 0;
-    }
+        reset_expa(i);
 }
