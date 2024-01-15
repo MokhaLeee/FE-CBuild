a195fd99129f22d583fb04d0435225c415f0294f
MokhaLeee <nkulyc@163.com>
2024-01-15 11:50:17 +0000

    reset fix

--- Wizardry/Core/BattleSys/Source/BattleHit.c
+++ Wizardry/Core/BattleSys/Source/BattleHit.c
@@ -365,12 +365,12 @@ bool BattleGenerateHit(struct BattleUnit * attacker, struct BattleUnit * defende
         }
 #endif
 
-        if (defender->unit.curHP == 0)
+        if (gBattleTarget.unit.curHP == 0)
         {
 #if CHAX
             gBattleActorGlobalFlag.enimy_defeated = true;
 
-            if (attacker == &gBattleActor && CheckBattleSkillActivte(&gBattleActor, &gBattleTarget, SID_Galeforce, GetUnitSkill(GetUnit(gBattleActor.unit.index))))
+            if (CheckBattleSkillActivte(&gBattleActor, &gBattleTarget, SID_Galeforce, GetUnitSkill(GetUnit(gBattleActor.unit.index))))
                 gBattleActorGlobalFlag.skill_activated_galeforce = true;
 #endif
             gBattleHitIterator->info |= BATTLE_HIT_INFO_KILLS_TARGET;
